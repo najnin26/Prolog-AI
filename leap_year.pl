25141df23aaa143243d32a30758606f6a4902810
najnin26 <najninshirin26@gmail.com>
2024-10-31 21:52:04 +0600

    Update leap_year.pl

--- leap_year.pl
+++ leap_year.pl
@@ -5,8 +5,8 @@ check_leap_year :-
 
 is_leap_year(Year) :-
     (Year mod 4 =:= 0, Year mod 100 =\= 0 ; Year mod 400 =:= 0) ->
-    format('~w is a leap year.', [Year]);
-    format('~w is not a leap year.', [Year]).
+    writef('%w is a leap year.', [Year]);
+    writef('%w is not a leap year.', [Year]).
 
 
 
