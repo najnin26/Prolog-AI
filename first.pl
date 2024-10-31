7f2909b4031801ff25cad6829567880d329b2139
najnin26 <najninshirin26@gmail.com>
2024-10-31 13:20:58 +0600

    1st

---
+++ first.pl
@@ -0,0 +1,16 @@
+name(shirin).
+name(abin).
+man(adam).
+girl(jerry).
+like(blue).
+like(adam,pizza).
+like(abin,banana).
+eat(adam,apple).
+
+parent(pam,bob).
+parent(tom,bob).
+parent(tom,liz).
+parent(bob,ann).
+parent(pat,jim).
+parent(tom,kim).
+parent(bob,pat).
